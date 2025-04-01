d9f3a8d0e04e7b9dc63f900f79d230992a686e52
Noah_FGC <laplaze.noah@gmail.com>
2025-04-01 14:02:17 +0200

    ajout class et début de l'application

--- battle_droids/lib/main.dart
+++ battle_droids/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:provider/provider.dart';
 
 
 void main() {
@@ -10,116 +10,58 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyAppState extends ChangeNotifier {
+  var ptsCompetence =10;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  void refresh(){
+  notifyListeners();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var appState = context.watch<MyAppState>();
+    var testcompetence = appState.ptsCompetence; 
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Column(
+        children: [
+          Align(
+          alignment: Alignment.center,
+          child : Text('Bienvenue sur BattleDroids'),
+          ),
+          SizedBox(height : 20),
+          Align(
+          alignment: Alignment.center,
+          child : Text('Créez votre robot, achetez-lui des équipements et que la bataille commence !!!'),
+          ),
+          SizedBox(height : 20),
+        ElevatedButton( //ajout d'un button
+          onPressed: () {
+            appState.ptsCompetence += 1;
+            print('button fouiller pressed!'); //console button pressed
+            appState.refresh();
+          },
+          child: Text('Test'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ]),
+      
     );
   }
 }
@@ -130,7 +72,7 @@ class Robot {
   int _attack;
   int _defense;
   int _argent;
-
+  List<Objet> inventaire=[];
 
 
   Robot(this._nom, this._health, this._attack, this._defense, this._argent);
@@ -156,33 +98,59 @@ class Robot {
     this._nom = nom;
   }
 
-  void setHealth(int health) {
-    this._health = health;
+  void upHealth(int health) {
+    this._health += health;
+  } 
+
+  void downHealth(int health) {
+    this._health -= health;
   } 
 
   void setAttack(int attack) {
-    this._attack = attack;
+    this._attack += attack;
   } 
 
   void setDefense(int defense) {
-    this._defense = defense;
+    this._defense += defense;
   } 
 
-  void setArgent(int argent) {
-    this._argent = argent;
+  void upArgent(int argent) {
+    this._argent += argent;
+  }
+
+  void downArgent(int argent) {
+    this._argent -= argent;
   }
 
   int getArgent() {
     return this._argent;
-  } 
+  }
+
+
+  int deleteObjFromInventaire(int obj){
+    inventaire.removeAt(obj);
+    return 1; //obj supprimer
+  }
+
+  String afficherInventaire(){
+    String liste = "";
+    for(int i=0; i<inventaire.length; i++){ //listeObjet[0]
+      String nomtest = inventaire[i].getNom();
+      int prixtest = inventaire[i].getPrix();
+      liste += "$nomtest : $prixtest\n";
+    }
+    return liste;
+  }
+
+
 }
 
 
 class Objet{
   Map<String, Objet> _listeObjet = {
-    "potion de soin": Objet("potion", 10, 1, 10),
-    "Armure en Fer": Objet("potion", 10, 2, 10),
-    "Pistolet Mitrailleur": Objet("potion", 10, 3, 10), //...
+    "Potion de soin": Objet("Potion de soin", 10, 1, 10),
+    "Armure en Fer": Objet("Armure en Fer", 10, 2, 10),
+    "Parchemin magique": Objet("Parchemin magique", 10, 3, 10), //...
   };
   int prix;
   String nom;
@@ -207,10 +175,69 @@ class Objet{
     return this.point;
   }
 
-  void AfficherListeObjet(){
+  String afficherListeObjet(){
+    String liste = "";
     for(int i=0; i<_listeObjet.length; i++){ //listeObjet[0]
-      
+      String nomtest = _listeObjet.keys.elementAt(i);
+      int prixtest = _listeObjet.values.elementAt(i).getPrix();
+      liste += "$nomtest : $prixtest\n";
+    }
+    return liste;
+  }
+}
+
+
+class Battle{
+
+  int acheterObjet(Map<String, Objet> listeShop, int obj, Robot robot){ //obj numéro objet
+    int retour =1; //pas assez d'argent
+      if(listeShop.values.elementAt(obj).getPrix() < robot.getArgent()){
+        robot.inventaire.add(listeShop.values.elementAt(obj));
+        retour = 2; //objet acheter
+        String nomObj = listeShop.values.elementAt(obj).getNom();
+        robot.downArgent(listeShop.values.elementAt(obj).getPrix());//retire argent robot
+        listeShop.remove(nomObj);
+      }
+    return retour;  
+  }
+
+
+  int afficherShop(){
+    Objet test = new Objet("test", 1, 1, 1);
+    test.afficherListeObjet();
+    return 1;
+  }
+
+
+  int utiliserObjet(Robot robot, int obj){//obj numéro objet
+    int test=0;
+    Objet objetUse = robot.inventaire[obj];
+    int pointAction = objetUse.getPoint();
+    if(objetUse.getType() == 1){ //soin
+      robot.upHealth(pointAction);
+      test = test+1;
+    }else if(objetUse.getType() == 2){ //defense
+      robot.setDefense(pointAction);
+      test = test+1;
+    }else if(objetUse.getType() == 3){ //attack
+      robot.setAttack(pointAction);
+      test = test+1;
     }
 
+    if(test == 1){ //supprime l'objet une fois utiliser
+      robot.deleteObjFromInventaire(obj);
+    }
+    return test; //objet supprimer/utiliser avec succès
   }
+
+  int attackRobot(Robot robot1, Robot robot2){ //robot 1 attack le 2
+    int ptsAttack = robot1.getAttack(); //attack robot
+    int ptsHealth = robot2.getHealth(); //pts vie robot2
+    int calc = ptsAttack - robot2.getDefense(); //calcul attack moins défense robot2
+    robot2.downHealth(calc); //attack le robot2
+    return 1; //robot attacker avec succès
+  }
+
+
+
 }
